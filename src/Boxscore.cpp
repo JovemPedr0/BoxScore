95a88b6fc0198300c45f7bb465a1fcc63e979602
Pedro Lucas <plvm0220@gmail.com>
2021-09-08 09:13:24 -0300

    CorrigindoATabela

--- src/Boxscore.cpp
+++ src/Boxscore.cpp
@@ -12,18 +12,20 @@ void Boxscore::getPtsTotal(Time team_a, Time team_b){
         nPontuacaoTimeB += team_b.nPontosMarcados[i];
     }
 
-    cout << team_a.nomeTime << " X " << team_b.nomeTime << endl;
-    cout << nPontuacaoTimeA << " X " << nPontuacaoTimeB << endl;
+    cout << "------------------------------------------------------" << endl;
+    cout << "|" << "\t" << team_a.nomeTime << " X " << team_b.nomeTime << "|" << endl;
+    cout << "|" << "\t" << nPontuacaoTimeA << " X " << nPontuacaoTimeB << "|" << endl;
+    cout << "------------------------------------------------------" << endl;
 }
 
 void Boxscore::getResumoJogo(Time team_a){
     printf("RESUMO DO JOGO\n");
     cout << team_a.nomeTime << endl;
-    printf("------------------------------------------------------------------------------------------------------------------------------------------------------\n");
-    printf(" Nome do Jogador Numero do Jogador\tPontos\tAssitências\tRoubos\tRebotes\tTurnovers\tFaltas\n");
-    printf("------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------------------------------------------------------\n");
+    printf(" Nome do Jogador\tNumero do Jogador\t\tPontos\t\tAssitências\t\tRoubos\t\tRebotes\t\tTurnovers\t\tFaltas\n");
+    printf("------------------------------------------------------------------------------------------------------------------\n");
 
     for (int i = 0; i < MAX_JOGADORES; i++){
-       cout << team_a.nomeJogador[i] << "\t" << team_a.numeroJogador[i] << "\t" << team_a.nPontosMarcados[i] << "\t" << team_a.nAssists[i] << "\t" << team_a.nRoubos[i] << "\t" << team_a.nRebotes[i] << "\t" <<team_a.nTurnovers[i] << "\t" << team_a.nFaltas[i] << endl;
+       cout << team_a.nomeJogador[i] << "\t" << team_a.numeroJogador[i] << "\t\t" << team_a.nPontosMarcados[i] << "\t\t" << team_a.nAssists[i] << "\t\t" << team_a.nRoubos[i] << "\t\t" << team_a.nRebotes[i] << "\t\t" <<team_a.nTurnovers[i] << "\t\t" << team_a.nFaltas[i] << endl;
     }
 }
